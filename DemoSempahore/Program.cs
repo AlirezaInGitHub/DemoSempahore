bdd26ffde84f3efe886fd04b4b4fc70a61bf7e23
Alireza Mokhtaripour <alireza.mokhtaripour@readify.net>
2018-12-26 19:19:32 +1100

    Add project files.

---
+++ DemoSempahore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoSempahore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
